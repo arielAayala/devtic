51721d57063178d24b84c85625624e8ed5c54021
arielAayala <ariel170802@gmail.com>
2023-09-15 22:20:27 -0300

    demandasForm y personasInvolucradasForm

--- front/src/components/demandaForm/demandaForm.jsx
+++ front/src/components/demandaForm/demandaForm.jsx
@@ -94,37 +94,36 @@ function DemandaForm() {
 						<p className="mt-1 text-sm leading-6 text-gray-600">
 							Aqui se narran todos los detalles de los casos.
 						</p>
+						<div className="sm:col-span-4">
+							<label
+								htmlFor="motivoDemanda"
+								className="block text-sm font-medium leading-6 text-gray-900"
+							>
+								Motivo
+							</label>
+							<div className="mt-2">
+								<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+									<span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
+										/
+									</span>
+									<input
+										onChange={handleChange}
+										type="text"
+										name="motivoDemanda"
+										id="motivoDemanda"
+										autoComplete="username"
+										className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+										placeholder="Ingrese un motivo aqui"
+									/>
+								</div>
+							</div>
+						</div>
 						<div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-4">
 							<div className="sm:col-span-4">
 								<PersonaInvolucradaForm />
 							</div>
 						</div>
 						<div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-							<div className="sm:col-span-4">
-								<label
-									htmlFor="motivoDemanda"
-									className="block text-sm font-medium leading-6 text-gray-900"
-								>
-									Motivo
-								</label>
-								<div className="mt-2">
-									<div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-										<span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
-											/
-										</span>
-										<input
-											onChange={handleChange}
-											type="text"
-											name="motivoDemanda"
-											id="motivoDemanda"
-											autoComplete="username"
-											className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-											placeholder="Ingrese un titulo aqui"
-										/>
-									</div>
-								</div>
-							</div>
-
 							<div className="col-span-full">
 								<label
 									htmlFor="relatoDemanda"
@@ -225,12 +224,6 @@ function DemandaForm() {
 					</div>
 				</div>
 				<div className="mt-6 flex items-center justify-end gap-x-6">
-					<button
-						type="button"
-						className="text-sm font-semibold leading-6 text-gray-900"
-					>
-						Cancelar
-					</button>
 					<button
 						type="submit"
 						className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
--- front/src/components/personaInvolucradaForm/personaInvolucradaForm.jsx
+++ front/src/components/personaInvolucradaForm/personaInvolucradaForm.jsx
@@ -1,18 +1,43 @@
-import React, { useEffect, useState } from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 
 function PersonaInvolucradaForm() {
 	const [loader, setLoader] = useState(false);
 	const [localidades, setLocalidades] = useState([]);
 
-	const [persona, setPersona] = useState({
+	const [demandante, setDemandante] = useState({
 		nombrePersona: null,
 		dniPersona: null,
 		localidad: null,
 		telefono: null,
 		correo: null,
 		domicilio: null,
-		demandante: null,
+		demandante: true,
+		curso: null,
+		grado: null,
+		docente: null,
+		idParentesco: null,
 	});
+
+	const [alumno, setAlumno] = useState({
+		nombrePersona: null,
+		dniPersona: null,
+		localidad: null,
+		telefono: null,
+		correo: null,
+		domicilio: null,
+		demandante: false,
+		curso: null,
+		grado: null,
+		docente: null,
+		idParentesco: null,
+	});
+
+	const handleChange = (e) => {
+		setDemandante({ ...demandante, [e.target.name]: e.target.value });
+	};
+
+	console.log(demandante);
 
 	const listarLocalidades = () => {
 		fetch("http://localhost/devtic/api/ListarLocalidades.php", {
@@ -31,8 +56,6 @@ function PersonaInvolucradaForm() {
 				console.log(error);
 			});
 	};
-
-	const handleChange = () => {};
 
 	useEffect(() => {
 		listarLocalidades();
@@ -54,13 +77,14 @@ function PersonaInvolucradaForm() {
 				<div className="relative z-0 w-full mb-6 group">
 					<input
 						type="text"
-						name="floating_last_name"
+						name="nombrePersona"
 						className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 						placeholder=" "
+						onChange={handleChange}
 						required
 					/>
 					<label
-						htmlFor="floating_last_name"
+						htmlFor="nombrePersona"
 						className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
 					>
 						Nombre y apellido
@@ -69,13 +93,14 @@ function PersonaInvolucradaForm() {
 				<div className="relative z-0 w-full mb-6 group">
 					<input
 						type="text"
-						name="floating_last_name"
+						name="dniPersona"
 						className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 						placeholder=" "
+						onChange={handleChange}
 						required
 					/>
 					<label
-						htmlFor="floating_last_name"
+						htmlFor="dniPersona"
 						className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
 					>
 						Documento
@@ -87,13 +112,14 @@ function PersonaInvolucradaForm() {
 					<input
 						type="tel"
 						pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-						name="floating_phone"
+						name="telefono"
 						className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 						placeholder=" "
+						onChange={handleChange}
 						required
 					/>
 					<label
-						htmlFor="floating_phone"
+						htmlFor="telefono"
 						className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
 					>
 						Número De Teléfono
@@ -102,28 +128,14 @@ function PersonaInvolucradaForm() {
 				<div className="relative z-0 w-full mb-6 group">
 					<input
 						type="text"
-						name="floating_company"
+						name="domicilio"
 						className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 						placeholder=" "
+						onChange={handleChange}
 						required
 					/>
 					<label
-						htmlFor="floating_company"
-						className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-					>
-						Dirección De Correo
-					</label>
-				</div>
-				<div className="relative z-0 w-full mb-6 group">
-					<input
-						type="text"
-						name="floating_company"
-						className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-						placeholder=" "
-						required
-					/>
-					<label
-						htmlFor="floating_company"
+						htmlFor="domicilio"
 						className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
 					>
 						Domicilio
@@ -140,7 +152,7 @@ function PersonaInvolucradaForm() {
 						<select
 							onChange={handleChange}
 							type="search"
-							name="idOrganizacion"
+							name="idLocalidad"
 							className="block border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 							autoComplete="organization"
 						>
@@ -166,8 +178,9 @@ function PersonaInvolucradaForm() {
 					<div className="flex items-center mb-4">
 						<input
 							type="radio"
-							name="countries"
-							value="Spain"
+							name="idParentesco"
+							value="1"
+							onChange={handleChange}
 							className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:bg-gray-700 dark:border-gray-600"
 						/>
 						<label
@@ -181,8 +194,9 @@ function PersonaInvolucradaForm() {
 					<div className="flex items-center mb-4">
 						<input
 							type="radio"
-							name="countries"
-							value="USA"
+							onChange={handleChange}
+							name="idParentesco"
+							value="2"
 							className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
 						/>
 						<label
@@ -196,8 +210,9 @@ function PersonaInvolucradaForm() {
 					<div className="flex items-center mb-4">
 						<input
 							type="radio"
-							name="countries"
-							value="Germany"
+							name="idParentesco"
+							onChange={handleChange}
+							value="3"
 							className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
 						/>
 						<label
@@ -207,22 +222,159 @@ function PersonaInvolucradaForm() {
 							Tutor Legal
 						</label>
 					</div>
-
-					<div className="flex items-center mb-4">
-						<input
-							type="radio"
-							name="countries"
-							value="Spain"
-							className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:bg-gray-700 dark:border-gray-600"
-						/>
-						<label
-							htmlFor="country-option-3"
-							className="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-						>
-							Hijo o Hija
-						</label>
-					</div>
 				</fieldset>
+			</div>
+			<div className="space-y-12 mt-10">
+				<div className="border-b border-gray-900/10 pb-3">
+					<h2 className="text-base font-semibold leading-7 text-gray-900">
+						Datos del alumno
+					</h2>
+					<p className="mt-1 text-sm leading-6 text-gray-600">
+						Aqui se especifican los datos del alumno.
+					</p>
+				</div>
+			</div>
+			<div className="grid md:grid-cols-2 md:gap-6 mt-4">
+				{demandante.idParentesco == 1 ? null : (
+					<>
+						<div className="relative z-0 w-full mb-6 group">
+							<input
+								type="text"
+								name="floating_last_name"
+								className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+								placeholder=" "
+								required
+							/>
+							<label
+								htmlFor="floating_last_name"
+								className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+							>
+								Nombre y apellido
+							</label>
+						</div>
+						<div className="relative z-0 w-full mb-6 group">
+							<input
+								type="text"
+								name="floating_last_name"
+								className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+								placeholder=" "
+								required
+							/>
+							<label
+								htmlFor="floating_last_name"
+								className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+							>
+								Documento
+							</label>
+						</div>
+						<div className="relative z-0 w-full mb-6 group">
+							<input
+								type="text"
+								name="floating_company"
+								className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+								placeholder=" "
+								required
+							/>
+							<label
+								htmlFor="floating_company"
+								className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+							>
+								Domicilio
+							</label>
+						</div>
+						<div className="relative z-0 w-full mb-6 group">
+							<label
+								htmlFor="idOrganizacion"
+								className="block text-sm font-medium leading-6 text-gray-900"
+							>
+								Localidad del domicilio
+							</label>
+							<div className="mt-2">
+								<select
+									onChange={handleChange}
+									type="search"
+									name="idOrganizacion"
+									className="block border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+									autoComplete="organization"
+								>
+									<option value={0}>Seleccione una opción</option>
+									{loader ? (
+										localidades.map((i) => (
+											<option
+												key={i.idLocalidad}
+												value={i.idLocalidad}
+											>
+												{i.nombreLocalidad}
+											</option>
+										))
+									) : (
+										<option>Cargando localidades</option>
+									)}
+								</select>
+							</div>
+						</div>
+					</>
+				)}
+
+				<div className="relative z-0 w-full mb-6 group">
+					<label
+						htmlFor="idTipo"
+						className="block text-sm font-medium leading-6 text-gray-900"
+					>
+						Grado
+					</label>
+					<div className="mt-2">
+						<select
+							onChange={handleChange}
+							name="idTipo"
+							id="idTipo"
+							className="block border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						>
+							<option value={0}>Seleccione una opción</option>
+							<option value={1}>Invitación</option>
+							<option value={2}>Solicitud</option>
+							<option value={3}>Expediente</option>
+						</select>
+					</div>
+				</div>
+
+				<div className="relative z-0 w-full mb-6 group">
+					<label
+						htmlFor="idTipo"
+						className="block text-sm font-medium leading-6 text-gray-900"
+					>
+						Turno
+					</label>
+					<div className="mt-2">
+						<select
+							onChange={handleChange}
+							name="idTipo"
+							id="idTipo"
+							className="block border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						>
+							<option value={0}>Seleccione una opción</option>
+							<option value={1}>Matutino</option>
+							<option value={2}>Vespertino</option>
+							<option value={3}>Nocturno</option>
+							<option value={4}>Especial</option>
+						</select>
+					</div>
+				</div>
+				<div className="relative z-0 w-full mb-6 group">
+					<input
+						type="text"
+						name="floating_last_name"
+						className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+						placeholder=" "
+						required
+					/>
+					<label
+						htmlFor="floating_last_name"
+						className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+					>
+						Docente
+					</label>
+				</div>
 			</div>
 		</>
 	);
